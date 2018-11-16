c7ef5586023d9438df771289dcaaf1d4a7fdb38f
HurricanGod <1807363937@qq.com>
2018-11-16 18:39:23 +0800

    添加一个供源码调试的类

---
+++ common/src/main/java/cn/hurrican/common/config/CustomBeanFactoryPostProcessor.java
@@ -0,0 +1,28 @@
+package cn.hurrican.common.config;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+
+/**
+ * @Author: Hurrican
+ * @Description:
+ * @Date 2018/11/15
+ * @Modified 10:05
+ */
+
+public class CustomBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
+    /**
+     * Modify the application context's internal bean factory after its standard
+     * initialization. All bean definitions will have been loaded, but no beans
+     * will have been instantiated yet. This allows for overriding or adding
+     * properties even to eager-initializing beans.
+     *
+     * @param beanFactory the bean factory used by the application context
+     * @throws BeansException in case of errors
+     */
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+
+    }
+}
